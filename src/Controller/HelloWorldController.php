3d0c1b0220e2a93d138c0b9ea0c2bd2b4bf3818c
Paul Myers <sellpahomes@gmail.com>
2026-03-23 20:27:03 -0400

    updated commit

--- src/Controller/HelloWorldController.php
+++ src/Controller/HelloWorldController.php
@@ -1,50 +1,52 @@
 <?php
 
 namespace Drupal\hello_world\Controller;
+
 use Drupal\Core\Controller\ControllerBase;
 use Drupal\hello_world\HelloWorldSalutation;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+
 /**
  * Controller for the salutation message.
  */
 class HelloWorldController extends ControllerBase {
+
   /**
    * The salutation service.
+   *
    * @var \Drupal\hello_world\HelloWorldSalutation
    */
   protected $salutation;
 
   /**
-   * Constructs a HelloWorldController object.
+   * HelloWorldController constructor.
    *
    * @param \Drupal\hello_world\HelloWorldSalutation $salutation
    *   The salutation service.
    */
-
   public function __construct(HelloWorldSalutation $salutation) {
     $this->salutation = $salutation;
   }
+
   /**
    * {@inheritdoc}
    */
-
   public static function create(ContainerInterface $container) {
     return new static(
       $container->get('hello_world.salutation')
     );
   }
 
-
   /**
    * Hello World.
-   * 
+   *
    * @return array
    *   Our message.
    */
   public function helloWorld() {
     return [
-      '#type' => 'markup',
       '#markup' => $this->salutation->getSalutation(),
     ];
   }
+
 }
